bd572bae8a286fde8490530b40ef028f921bb930
aakashmohole <aakashmohole@gmail.com>
2026-05-05 22:57:58 +0530

    changes in templete project name

--- template.py
+++ template.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import logging
 
 logging.basicConfig(level=logging.INFO, format='[%asctime)s]: %(message)s:')
-project_name = "Wine Quality"
+project_name = "wine_quality_project"
 
 
 list_of_files=[
